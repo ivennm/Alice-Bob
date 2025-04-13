c044912f6973263c8b469ba9659231e89ffdd436
Ivan Mendez <166038157+ivennm@users.noreply.github.com>
2025-04-12 21:13:30 -0400

    more more more data

--- data/opener.py
+++ data/opener.py
@@ -1,10 +1,10 @@
 import pickle
 
 # Open the pickle file in read-binary mode
-with open('data/wigner_fock_one.pickle', 'rb') as file:
+with open('data/wigner_cat_plus_113.pickle', 'rb') as file:
     # Load the data from the pickle file
     data = pickle.load(file)
-with open('wigner_fock1.txt', 'w') as file:
+with open('data_txt/wigner_cat_plus_113.txt', 'w') as file:
     # Write the data to a text file
     for item in data:
         file.write(f"{item}\n")
